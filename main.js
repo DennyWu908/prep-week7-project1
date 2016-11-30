9e4794bfa2300fd467f97a746296286f5cd619a8
Denny Wu <denny.wu87@gmail.com>
2016-11-29 21:11:24 -0800

    Add google map code to html file

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ Have fun!
 
 var map;
 function initMap() {
-map = new google.maps.Map(document.getElementById('map'), {
-	center: {lat: -34.397, lng: 150.644}, zoom: 8
+	map = new google.maps.Map(document.getElementById('map'), {
+		center: {lat: -34.397, lng: 150.644}, zoom: 8
 	});
 }
