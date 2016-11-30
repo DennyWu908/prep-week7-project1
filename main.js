4bd1a946ff82bb6043c049a92cf36d033fcfc7bc
Denny Wu <denny.wu87@gmail.com>
2016-11-29 20:15:11 -0800

    Added code to html and js files

--- main.js
+++ main.js
@@ -15,3 +15,9 @@ Have fun!
 =======================================================
 */
 
+var map;
+function initMap() {
+map = new google.maps.Map(document.getElementById('map'), {
+	center: {lat: -34.397, lng: 150.644}, zoom: 8
+	});
+}
